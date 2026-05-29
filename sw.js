3f7252872555dcb67e6d1315391565ad5bd04510
Yasha Kul <yashakul@yashas-mbp.home>
2026-05-29 22:16:27 +0200

    bust cache v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kaizen-v1';
+const CACHE = 'kaizen-v2';
 const SHELL = ['./app.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
