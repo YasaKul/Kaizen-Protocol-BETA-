aa11a48d95157c482ccdaf0355e3f5e8bb612c5d
Yasha Kul <yashakul@yashas-mbp.home>
2026-05-30 09:21:01 +0200

    fix journal mobile layout - single column with back button

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kaizen-v4';
+const CACHE = 'kaizen-v5';
 const SHELL = ['./app.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
