e86a3575c7cd10fc9bc7f0f0d1411f8de8ca56de
Yasha Kul <yashakul@yashas-mbp.home>
2026-05-30 09:12:04 +0200

    add journal/pillars/contact to client nav + scrollable

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kaizen-v2';
+const CACHE = 'kaizen-v3';
 const SHELL = ['./app.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
