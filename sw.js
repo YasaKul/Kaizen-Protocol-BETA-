b22ea27b8cd1fddab39dc2b0d379fa30b3db6838
Yasha Kul <yashakul@yashas-mbp.home>
2026-05-30 09:14:19 +0200

    auto-select input value on focus for all number fields

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kaizen-v3';
+const CACHE = 'kaizen-v4';
 const SHELL = ['./app.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
